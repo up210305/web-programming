0dae8553856233c91be6ad213a038dd04c44eb73
UP210305 <up210305@alumnos.upa.edu.mx>
2024-02-18 15:39:52 -0600

    Updates

---
+++ count-clicks/app.js
@@ -0,0 +1,20 @@
+// number
+let x = 1;
+let y = 10;
+
+let c = x - y;
+//alert(c);
+
+// string
+let name = "asasa";
+let name1 = 'sadsa';
+let name2 = `dsasdsad`;
+
+// boolean
+let a = true && true || false;
+let b = !false;
+let min_number = 1;
+
+let button = document.getElementById("btn");
+
+console.log(button);
